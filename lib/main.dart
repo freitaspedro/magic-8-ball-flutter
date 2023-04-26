a27fddd2feaf522370d87314e17bcdced661e0de
freitaspedro <pfreitas@id.uff.br>
2023-04-26 16:33:49 -0300

    add random ball page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() {
+  return
+    runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var ballPage = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() { ballPage = Random().nextInt(5) + 1; });
+              },
+              child: Image.asset('images/ball$ballPage.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
